24dea0363c241c89109ba48679d12eac35e8a10e
Scott <scott@scotttrinh.com>
2020-12-21 13:36:14 -0500

    Fix Tailwind config that was broken during html -> md

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./src/**/*.html"],
+  purge: ["./src/**/*.md"],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
